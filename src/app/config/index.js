772181a50e49d34e245edede15ff4e3ac4a1d691
Erik Evenson <erik.e.evenson@gmail.com>
2015-06-16 21:23:15 -0500

    checkpoint

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -10,18 +10,25 @@ angular.module('app')
         templateUrl: 'app.home.html'
       })
       .state('home.list', {
-        url: '/list',
-        templateUrl: 'app.home.list.html',
-        controller: function($scope) {
-          $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-        }
+        // url: '/list',
+        template: 'A list'
       })
       .state('home.paragraph', {
-        url: '/paragraph',
+        // url: '/paragraph',
         template: 'I could sure use a drink right now.'
       })
       .state('about', {
-        // TBD
+        url: '/about',
+        views: {
+            // the main template will be placed here (relatively named)
+            '': { templateUrl: 'app.about.html' },
+
+            // the child views will be defined here (absolutely named)
+            'columnOne@about': { template: 'Look I am a column 1!' },
+
+            // for column two, we'll define a separate controller 
+            'columnTwo@about': { template: 'Look I am a column 2!' }
+        }
       });
   }]);
 
